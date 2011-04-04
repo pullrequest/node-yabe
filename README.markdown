3fca57c37af661e64315e8c24a85b71fa2dc2f22
Mickael Daniel <daniel.mickael@gmail.com>
2011-04-05 01:07:05 +0200

    further work

---
+++ README.markdown
@@ -0,0 +1,17 @@
+# Node Yabe
+
+A simple (but yet another) blog engine written in node.js. It basically takes the `àrticles/` folder full of markdown post and serves them as a website.
+
+npm is not used yet, the dependencies are built-in in this repo.
+
+connect as a middleware stack (namely for router). nodejs v0.4.x
+
+h5b [node.js](https://github.com/paulirish/html5-boilerplate-server-configs/blob/master/node.js)(thanks xonecas!) server configuration is used.
+
+[node-jqtpl](https://github.com/kof/node-jqtpl) for templating.
+
+markdown and prettify node module are node ports of showdown and prettify syntax highlighter by [creationix](https://github.com/creationix) for the [Wheat](https://github.com/creationix/wheat) blog engine.
+
+This is a work in progress (and mostly just fun).
+
+Yabe mainly due to my lack of inspiration when it comes to chose a name. I chose to create yet another blog engine. It is a reference to this [play](http://www.playframework.org/documentation/1.0.1/guide1#aTheprojecta) framework sample project.
--- renderer.js
+++ renderer.js
@@ -1,11 +1,57 @@
 var fs = require('fs'),
 jqtpl = require('./lib/node-jqtpl'),
-markdown = require('./lib/markdown');
+Markdown = require('./lib/markdown'),
+prettify = require('./lib/prettify'),
+QueryString = require('querystring'),
+res = require('http').ServerResponse.prototype;
+
+res.render = function(target, data) {
+  
+  var partial = jqtpl.tmpl('tmpl.' + target, data),
+  layout = jqtpl.tmpl('tmpl.layout', {content: partial});
+  
+  // prettify snippets of code
+  layout = layout.replace(/<pre><code>[^<]+<\/code><\/pre>/g, function (code) {
+    code = code.match(/<code>([\s\S]+)<\/code>/)[1];
+    code = prettify.prettyPrintOne(code);
+    return "<pre><code>" + code + "</code></pre>";
+  });
+  
+  this.writeHead(200, { 
+      'content-type': 'text/html',
+      'content-length': layout.length
+  });
+  
+  this.end(layout);
+};
+
 
 var Renderers = module.exports = (function(o) {
     
     // Cache templates on application startup
-    var addTemplate = function(file) {
+    var parseProps = function(markdown) {
+      var props = { };
+
+      // Parse out headers
+      var match;
+      while(match = markdown.match(/^([a-z]+):\s*(.*)\s*\n/i)) {
+        var name = match[1].toLowerCase(),
+            value = match[2];
+        markdown = markdown.substr(match[0].length);
+        props[name] = value;
+      }
+      props.markdown = markdown;
+
+      if(props.categories !== undefined){
+        props.categories = props.categories.split(',').map(function(element){ 
+          return QueryString.escape(element.trim());
+        });
+      }
+
+      return props;
+    },
+    
+    addTemplate = function(file) {
         fs.readFile(__dirname + '/themes/default/' + file + '.html', 'utf8', function(err, tmpl) {
             if (err) throw err;
             
@@ -14,33 +60,85 @@ var Renderers = module.exports = (function(o) {
     };
     
     addTemplate('layout');
+    addTemplate('article');
+    addTemplate('index');
     
     return {
         index: function(req, res, next) {
-            console.log('index', this);
-            next();
+          
+          var render = function(articles) {
+            articles.sort(function(a, b) {
+              return (Date.parse(b.date)) - (Date.parse(a.date));
+            });
+            
+            res.render('index', {
+              articles: articles
+            });
+          };
+          
+          fs.readdir(__dirname + '/articles/', function(err, results) {
+            var files = [],
+            articles = [];
+          
+            results.forEach(function(filename, i) {
+              if (!(/\.markdown$/.test(filename))) {
+                return;
+              }
+              
+              files.push(filename);
+            });
+            
+            
+            ln = files.length - 1;
+            files.forEach(function(filename, i) {
+              if (!(/\.markdown$/.test(filename))) {
+                return;
+              }
+
+              fs.readFile(__dirname + '/articles/' + filename, function(err, markdown) {
+                var props;
+                if (err) throw err;
+
+                if (typeof markdown !== 'string') {
+                  markdown = markdown.toString();
+                }
+
+                props = parseProps(markdown);
+                props.name = filename.replace('.markdown', '');
+                props.markdown = Markdown.encode(props.markdown.substr(0, props.markdown.indexOf("##")));
+                
+                articles.push(props);
+                
+                if(i === ln) {
+                  render(articles);
+                }
+              });
+
+            });
+
+            
+          });
         },
         
-        category: function(){},
+        category: function() {},
         
-        article: function(req, res, next){
-            console.log('article ', req.params, __dirname);
-            
+        article: function(req, res, next) {
             var article = req.params.post + '.markdown';
             
             fs.readFile(__dirname + '/articles/' + article, 'utf8', function (err, body) {
+                var props;
                 if (err) return next(err);
                 
-                var result = jqtpl.tmpl('tmpl.layout', {content: markdown.encode(body)});
-                
-                res.writeHead(200, { 
-                    'content-type': 'text/html',
-                    'content-length': result.length
+                props = parseProps(body);
+                props.name = req.params.post;
+                res.render('article', {
+                  article: props,
+                  author: {name: props.author},
+                  content: Markdown.encode(body)
                 });
-                res.end(result);
-                
             });
         },
+        
         feed: function(){}
-    }
+    };
 });
